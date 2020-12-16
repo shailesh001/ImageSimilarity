d9766374f76664928e9804dedb197ab9f1899712
Shailesh Patel <shailesh001@mac.com>
2020-12-16 15:34:48 +0000

    Initial Commit

---
+++ ImageSimilarity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageSimilarity
+//
+//  Created by Shailesh Patel on 16/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageSimilarity/ImageSimilarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSimilarityApp.swift
+//  ImageSimilarity
+//
+//  Created by Shailesh Patel on 16/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ImageSimilarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
